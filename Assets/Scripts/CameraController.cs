89a631ad215c41326b70d4738447e6ee3525fe38
SergeyBirukov <birukovsergej121@gmail.com>
2021-04-22 19:56:52 +0500

    Add CameraController

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    private Vector3 position;
+
+    private void Awake()
+    {
+        if (!player)
+        {
+            player = FindObjectOfType<Player>().transform;
+        }
+    }
+
+    void Update()
+    {
+        position = player.position;
+        position.z = -10f;
+        transform.position = Vector3.Lerp(transform.position, position, Time.deltaTime);
+    }
+}
