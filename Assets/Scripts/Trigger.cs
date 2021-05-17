37acb3db31eb0a918dd48b8c828b280029cc947b
lessy2014 <lessik03@gmail.com>
2021-05-17 17:32:34 +0500

    Trigget event example has a homepoint parametr for trashMonster now

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Trigger : MonoBehaviour
 {
@@ -9,10 +11,12 @@ public class Trigger : MonoBehaviour
 
     public GameObject wall;
     public TrashMonster monster;
+
+    public Transform homePoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        monster.homePoint = homePoint;
     }
 
     // Update is called once per frame
