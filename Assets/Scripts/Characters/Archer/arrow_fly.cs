50aa892e3b62678baa52406b98223f4a776132a2
lessy2014 <lessik03@gmail.com>
2021-05-17 19:54:09 +0500

    High speed arrow velocity fix

--- Assets/Scripts/Characters/Archer/arrow_fly.cs
+++ Assets/Scripts/Characters/Archer/arrow_fly.cs
@@ -7,28 +7,28 @@ public class arrow_fly : MonoBehaviour
     public float speed = 10;
     public float distance = 0;
     public new Rigidbody2D rigidbody;
+    public CapsuleCollider2D Collider2D;
     void Start()
     {
         rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        Collider2D = gameObject.GetComponent<CapsuleCollider2D>();
         StartCoroutine(SelfDestroyAfterTime());
     }
-    void Update()
+    void FixedUpdate()
     {
-        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, distance);
-        if (!(hitInfo.collider is  null))
-        {
-            if (hitInfo.collider.gameObject.layer == 9)
-            {
-                hitInfo.collider.gameObject.GetComponent<Entity>().GetDamage(50);
-                Destroy(gameObject);
-            }
-            else if (hitInfo.collider.gameObject.layer == 8)
-            {
-                Destroy(gameObject);
-            }
-        }
-
         rigidbody.velocity = transform.right * speed;
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+            other.gameObject.GetComponent<Entity>().GetDamage(50);
+            Destroy(gameObject);
+        }
+        else if (other.gameObject.layer == 8 )
+        {
+            Destroy(gameObject);
+        }
     }
     
     IEnumerator SelfDestroyAfterTime()
