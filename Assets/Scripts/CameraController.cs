e3203ab9506830b3db71bceed19f923512bbb780
SergeyBirukov <birukovsergej121@gmail.com>
2021-05-21 20:33:52 +0500

    Add focus camera on basement by trigger

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,9 +7,13 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Player player;
     private Camera camera;
     private Vector3 position;
+    public bool isFocused;
+    public GameObject objectInFocus;
+    public static CameraController Instance;
 
     private void Awake()
     {
+        Instance = this;
         if (!player)
         {
             player = FindObjectOfType<Player>();
@@ -17,7 +21,7 @@ public class CameraController : MonoBehaviour
         camera = FindObjectOfType<Camera>();
     }
 
-    void Update()
+    private void FollowPlayer()
     {
         if (player.isWithSword && camera.orthographicSize < 5)
         {
@@ -29,10 +33,35 @@ public class CameraController : MonoBehaviour
             position.y += 5;
             camera.orthographicSize -= Time.deltaTime * 4;
         }
-           
+
         position = player.transform.position;
         position.z = -10f;
-        
+
         transform.position = Vector3.Lerp(transform.position, position, Time.deltaTime);
+
+    }
+
+    IEnumerable FocusTimer(int time)
+    {
+        yield return new WaitForSeconds(time);
+        print("end");
+        isFocused = false;
+    }
+
+    private void FocusOnObject(GameObject gameObject)
+    {
+        transform.position = Vector3.Lerp(this.transform.position, objectInFocus.transform.position, Time.deltaTime);
+        var distance = this.transform.position.magnitude - objectInFocus.transform.position.magnitude;
+        if (distance*distance < 0.2)
+            isFocused = false;
+        position.z = -10f;
+    }
+
+    void Update()
+    {
+        if (isFocused)
+            FocusOnObject(objectInFocus);
+        else
+            FollowPlayer();
     }
 }
