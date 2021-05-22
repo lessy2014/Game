5f895bcd2274b3262e6c1a2b3b973600cd8eb420
skypunkaudiowalker <kolesnikovv028@gmail.com>
2021-05-23 04:06:08 +0500

    add blackout for play button

--- Assets/MainMenuController.cs
+++ Assets/MainMenuController.cs
@@ -3,22 +3,49 @@ using System.Collections;
 using System.Collections.Generic;
 using Assets.Scripts;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+    public GameObject fill;
+    private Image background;
+
+    private void Awake()
+    {
+        fill.SetActive(false);
+        background = fill.GetComponent<Image>();
+    }
+
     public void Play()
     {
-        SceneManager.LoadScene("SampleScene");
+        StartCoroutine(Blackout());
     }
 
     public void Quit()
     {
-        
+        Application.Quit();
     }
 
     public void Settings()
     {
-        
+        StartCoroutine(Blackout());
+    }
+
+    private IEnumerator Blackout()
+    {
+        fill.SetActive(true);
+        var backgroundColor = background.color;
+        while (background.color.a < 1)
+        {
+            backgroundColor = new Color(backgroundColor.r,
+                backgroundColor.g,
+                backgroundColor.b,
+                backgroundColor.a + 0.05f);
+            background.color = backgroundColor;
+            yield return new WaitForSeconds(0.1f);
+        }
+        SceneManager.LoadScene("SampleScene");
     }
 }
