3ba3089fb2ed6e03c2de1f5a20b9b6d7b9ded283
skypunkaudiowalker <kolesnikovv028@gmail.com>
2021-05-23 02:35:30 +0500

    add new scene for main menu

--- Assets/MainMenuController.cs
+++ Assets/MainMenuController.cs
@@ -3,38 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Assets.Scripts;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    private Camera camera;
-    private Archer archer;
-    private Koldun koldun;
-    private Player player;
-    private GameObject UI;
-
-    private void Awake()
-    {
-        archer = FindObjectOfType<Archer>();
-        koldun = FindObjectOfType<Koldun>();
-        UI = GameObject.FindGameObjectWithTag("UserInterface");
-        camera = Camera.main;
-        
-        Time.timeScale = 0f;
-        koldun.GetComponent<Koldun>().enabled = false;
-        archer.GetComponent<Archer>().enabled = false;
-        UI.SetActive(false);
-        camera.orthographicSize = 1.3f;
-    }
-
-    private void Start()
-    {
-        player = FindObjectOfType<Player>();
-        player.input.Disable();
-    }
-
     public void Play()
     {
-
+        SceneManager.LoadScene("SampleScene");
     }
 
     public void Quit()
