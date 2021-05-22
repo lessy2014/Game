8bb13275919acc5d4ad132e321e5d2a603238b7e
skypunkaudiowalker <kolesnikovv028@gmail.com>
2021-05-23 04:45:34 +0500

    remove redudant elements from main menu map

--- Assets/MainMenuController.cs
+++ Assets/MainMenuController.cs
@@ -1,9 +1,5 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using Assets.Scripts;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
